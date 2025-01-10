2c9056a0400145583f16453b66688b6e963f525f
Divine Favour <favourme2580@gmail.com>
2025-01-10 06:53:21 +0100

    Add project files.

---
+++ OrderService/Program.cs
@@ -0,0 +1,39 @@
+using MassTransit;
+using SharedMessages.Messages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq://localhost");
+    });
+});
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapPost("/order", async (IBus bus, OrderRequest orderRequest) =>
+{
+    var orderPlacedMessage = new OrderPlaced(orderRequest.orderId, orderRequest.quantity);
+    await bus.Publish(orderPlacedMessage);
+    return Results.Created($"/order/{orderRequest.orderId}", orderPlacedMessage);
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+public record OrderRequest(Guid orderId, int quantity);
---
+++ ShippingService/Consumers/OrderPlacedConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using SharedMessages.Messages;
+
+namespace ShippingService.Consumers
+{
+    public class OrderPlacedConsumer : IConsumer<OrderPlaced>
+    {
+        public Task Consume(ConsumeContext<OrderPlaced> context)
+        {
+            Console.WriteLine($"Order {context.Message.OrderId} placed with quantity {context.Message.Quantity}");
+            return Task.CompletedTask;
+        }
+    }
+}
